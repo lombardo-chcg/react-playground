706c4310c7a54efb49d570a810c2d2f078a9d4d2
lombardo-chcg <dpa.lombardo@gmail.com>
2016-07-12 08:02:20 -0500

    Set up data path from action dispatch to map state to props

--- weather-app/src/actions/index.js
+++ weather-app/src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = 'e4ba75c2223346322e430c630cacae79';
+import { API_KEY } from '../utils/api_key'
+import axios from 'axios'
+
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
