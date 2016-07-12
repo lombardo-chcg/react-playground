5316588325df9fbdb2b154a83f346ef9a947a7a9
lombardo-chcg <dpa.lombardo@gmail.com>
2016-07-11 23:28:36 -0500

    Add redux promise

---
+++ weather-app/src/containers/search_bar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(eventObject) {
+    this.setState({ term: eventObject.target.value });
+  }
+
+  onFormSubmit(eventObject) {
+    eventObject.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder='get a 5 day forecast for your favorite cities'
+          className='form-control'
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className='input-group-btn'>
+          <button type='submit' className='btn btn-secondary'>Search</button>
+        </span>
+      </form>
+    )
+  }
+}
