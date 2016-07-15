f86adade5a63526c5a0e32d4742a83126489b9e9
lombardo-chcg <dpa.lombardo@gmail.com>
2016-07-14 22:21:32 -0500

    Finish weather app

--- weather-app/src/containers/weather_list.js
+++ weather-app/src/containers/weather_list.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Chart from '../components/chart'
+import GoogleMap from '../components/google_map'
+
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    // const temps = cityData.list.map(weather => weather.main.temp)
+    const temps = cityData.list.map(function(weather) {
+      return (Number(weather.main.temp)* 9/5 - 459.67);
+    });
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const lon = cityData.city.coord.lon;
+    const lat = cityData.city.coord.lat;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lat={lat} lon={lon} /></td>
+        <td><Chart data={temps} color="red" unit='F' /></td>
+        <td><Chart data={pressures} color="blue" unit='hPa' /></td>
+        <td><Chart data={humidities} color="green" unit='%' /></td>
       </tr>
     )
   }
@@ -18,9 +32,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (F)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
           <tbody>
